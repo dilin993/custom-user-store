3f5d51c861885db5b8ae06f3cbf85827311d8f45
dilin993 <dilin.dampahalage@gmail.com>
2019-01-11 16:57:31 +0530

    Get service provider whitelist from a user store property

--- src/main/java/com/wso2/carbon/custom/user/store/manager/CustomUserStoreManager.java
+++ src/main/java/com/wso2/carbon/custom/user/store/manager/CustomUserStoreManager.java
@@ -1,10 +1,14 @@
 package com.wso2.carbon.custom.user.store.manager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.common.util.IdentityApplicationManagementUtil;
+import org.wso2.carbon.user.api.Properties;
+import org.wso2.carbon.user.api.Property;
 import org.wso2.carbon.user.core.UserRealm;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.claim.ClaimManager;
@@ -13,6 +17,8 @@ import org.wso2.carbon.user.core.profile.ProfileConfigurationManager;
 
 public class CustomUserStoreManager extends JDBCUserStoreManager {
     private static Log log = LogFactory.getLog(CustomUserStoreManager.class);
+
+    public static final String PROPERTY_SERVICE_PROVIDERS_TO_ALLOW = "Service Providers to Allow";
 
     public CustomUserStoreManager() {
     }
@@ -33,10 +39,35 @@ public class CustomUserStoreManager extends JDBCUserStoreManager {
         if (log.isDebugEnabled()) {
             log.debug("Authentication request from SP=\'" + spName + "\'");
         }
-        if (!"Mainframe".equals(spName))   {
+        String spsToAllow = this.realmConfig.getUserStoreProperty(PROPERTY_SERVICE_PROVIDERS_TO_ALLOW);
+        ArrayList<String> spWhiteList = new ArrayList<String>();
+        if (spsToAllow != null) {
+            spWhiteList = new ArrayList<String>(Arrays.asList(spsToAllow.split(",")));
+        }
+        boolean isSpWhiteListed = false;
+        for (int i = 0; i < spWhiteList.size(); i++) {
+            if (spWhiteList.get(i) != null && spWhiteList.get(i).equals(spName)) {
+                log.debug("SP=\'" + spName + "\' is white listed");
+                isSpWhiteListed = true;
+                break;
+            }
+        }
+        if (!isSpWhiteListed) {
             return false;
         }
         return super.doAuthenticate(userName, credential);
     }
 
+    @Override
+    public Properties getDefaultUserStoreProperties() {
+        Properties properties = super.getDefaultUserStoreProperties();
+
+        ArrayList<Property> mandatoryProperties = new ArrayList<Property>(Arrays.asList(properties.
+                getMandatoryProperties()));
+
+        mandatoryProperties.add(new Property(PROPERTY_SERVICE_PROVIDERS_TO_ALLOW, "",
+                "List of service providers for which the user store should be allowed", null));
+        properties.setMandatoryProperties(mandatoryProperties.toArray(new Property[mandatoryProperties.size()]));
+        return properties;
+    }
 }
